da8985f03a271de8b0eaebfd4484523947d308c4
gukihuman <sokolovivanf@gmail.com>
2025-02-04 11:18:51 +0500

    add name

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex justify-between bg-stone-600 h-screen gap-1">
+  <div class="flex justify-between bg-stone-600 h-screen gap-1 p-1">
     <div
-      class="bg-stone-500 bg-circles w-[180px] flex flex-col gap-1 justify-between"
+      class="bg-stone-500 bg-circles w-[180px] flex flex-col justify-between rounded-lg overflow-hidden"
     >
       <button
         @click="createText"
@@ -9,18 +9,21 @@
       >
         new
       </button>
-      <div class="overflow-auto flex flex-col gap-1 flex-grow">
-        <button
-          v-for="([key, _], index) in textArray"
-          :class="
-            selectedTextI === index
-              ? 'bg-stone-400 text-stone-800'
-              : 'bg-stone-600 hover:bg-stone-700 text-stone-300'
-          "
-          @click="selectText(index)"
-        >
-          {{ key }}
-        </button>
+      <div ref="textArrayRef" class="flex-grow overflow-auto">
+        <div class="flex flex-col-reverse">
+          <button
+            v-for="([key, _], index) in textArray"
+            class="py-[2px] pr-1 text-left min-h-7 text-shadow truncate"
+            :class="
+              selectedTextI === index
+                ? 'pl-5 bg-gradient-to-r from-stone-600 to-transparent  text-stone-300'
+                : 'pl-3 hover:bg-gradient-to-r hover:from-stone-600/50 hover:to-transparent  text-stone-300'
+            "
+            @click="selectText(index)"
+          >
+            {{ key }}
+          </button>
+        </div>
       </div>
       <button
         @click="saveFile"
@@ -34,28 +37,62 @@
       >
         load
       </button>
+      <button
+        @click="restore"
+        class="w-full bg-stone-700 justify-self-end pb-1"
+        :class="
+          lastRemovedText
+            ? 'hover:bg-stone-800 text-stone-300'
+            : 'cursor-default text-stone-500'
+        "
+      >
+        restore
+      </button>
     </div>
-    <textarea
-      ref="textRef"
-      :disabled="selectedTextI === -1"
-      v-model="selectedText"
-      @input="onInput"
-      @scroll="handleScroll"
-      class="flex-grow h-full bg-lines bg-stone-400 py-7 px-8 resize-none focus:outline-none text-stone-800 text-xl"
-      :style="{ backgroundPositionY }"
-    ></textarea>
-    <div class="w-[180px] h-full bg-circles bg-stone-500"></div>
-    <div class="w-[180px] h-full bg-circles bg-stone-500"></div>
+    <div class="flex flex-col flex-grow">
+      <div class="flex flex-col flex-grow" v-show="selectedTextI > -1">
+        <div class="flex min-h-9 rounded-t-lg overflow-hidden justify-between">
+          <input
+            type="text"
+            v-model="selectedTextName"
+            @input="onInput"
+            class="flex-grow bg-circles bg-stone-500 text-center focus:outline-none text-xl text-stone-300"
+          />
+          <button
+            v-if="selectedTextI !== -1"
+            @click="remove"
+            class="bg-stone-700 px-5 justify-self-end text-stone-300 pb-1 hover:bg-stone-800"
+          >
+            remove
+          </button>
+        </div>
+        <textarea
+          ref="textRef"
+          v-model="selectedText"
+          @input="onInput"
+          @scroll="handleScroll"
+          class="flex-grow h-full bg-lines bg-stone-400 pt-7 p-8 resize-none focus:outline-none text-stone-800 text-xl rounded-b-lg"
+          :style="{ backgroundPositionY }"
+        ></textarea>
+      </div>
+    </div>
+    <div class="w-[180px] rounded-lg h-full bg-circles bg-stone-500"></div>
+    <div class="w-[180px] rounded-lg h-full bg-circles bg-stone-500"></div>
   </div>
 </template>
 <script setup>
 import { debounce } from "lodash"
 const LOCAL_STORAGE_KEY = "stone"
 const textRef = ref(null)
+const textArrayRef = ref(null)
 const textArray = ref([])
 const selectedTextI = ref(-1)
 const selectedText = ref("")
+const selectedTextName = ref("")
 const backgroundPositionY = ref("0px")
+const lastRemovedText = ref(null)
+onMounted(loadLocalStorageItem)
+
 function handleScroll(event) {
   backgroundPositionY.value = `-${event.target.scrollTop}px`
 }
@@ -65,27 +102,43 @@ function getStorage() {
     selectedTextI: selectedTextI.value,
   }
 }
-const onInput = debounce(() => {
+function onInput() {
+  textArray.value[selectedTextI.value][0] = selectedTextName.value
   textArray.value[selectedTextI.value][1] = selectedText.value
-  saveLocalStorageItem()
-}, 200)
-onMounted(loadLocalStorageItem)
+  debouncedSaveLocalStorageItem()
+}
 function selectText(index) {
+  if (selectedTextI.value === index) {
+    deselectText()
+    return
+  }
   selectedTextI.value = index
+  selectedTextName.value = textArray.value[index][0]
   selectedText.value = textArray.value[index][1]
   saveLocalStorageItem()
   nextTick(() => textRef.value.focus())
 }
+function deselectText() {
+  selectedTextI.value = -1
+  selectedText.value = ""
+  saveLocalStorageItem()
+}
 function createText() {
-  textArray.value.push([Math.random().toString().slice(2, 10), ""])
+  selectedTextName.value = Math.random().toString().slice(2, 10)
+  textArray.value.push([selectedTextName.value, ""])
   selectedTextI.value = textArray.value.length - 1
   selectedText.value = ""
   saveLocalStorageItem()
-  nextTick(() => textRef.value.focus())
+  nextTick(() => {
+    textRef.value.focus()
+    textArrayRef.value.scrollTop =
+      textArrayRef.value.clientHeight - textArrayRef.value.scrollHeight
+  })
 }
 function saveLocalStorageItem() {
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(getStorage()))
 }
+const debouncedSaveLocalStorageItem = debounce(saveLocalStorageItem, 200)
 function loadLocalStorageItem() {
   const rawStorage = localStorage.getItem(LOCAL_STORAGE_KEY)
   if (rawStorage) injectStorage(JSON.parse(rawStorage))
@@ -128,10 +181,21 @@ function loadFile() {
   fileInput.remove()
   saveLocalStorageItem()
 }
-// 📜 rename storage variable everywhere, think of a better solution
+function remove() {
+  lastRemovedText.value = textArray.value.splice(selectedTextI.value, 1)[0]
+  deselectText()
+}
+function restore() {
+  if (!lastRemovedText.value) return
+  textArray.value.push(lastRemovedText.value)
+  lastRemovedText.value = null
+  selectText(textArray.value.length - 1)
+}
 function injectStorage(storage) {
   textArray.value = storage.textArray
   selectedTextI.value = storage.selectedTextI
+  if (selectedTextI.value === -1) return
+  selectedTextName.value = textArray.value[selectedTextI.value][0]
   selectedText.value = textArray.value[selectedTextI.value][1]
   nextTick(() => textRef.value.focus())
 }
