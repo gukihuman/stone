9ef5b1175ce73ce7687f7e1c60b5684783d730fd
gukihuman <sokolovivanf@gmail.com>
2025-02-15 17:52:30 +0500

    add topics

--- README.md
+++ README.md
@@ -10,32 +10,7 @@ npm run build
 
 ## Structure
 
-### Events
-
-```json
-{
-  "9874122603798017": {
-    "name": "Philosophy Discussion",
-    "time": "2025-02-14",
-    "content": "To be, or not to be, that is the question",
-    "memory": ["to be", "not to be"],
-    "memoryLength": 2,
-    "memoryIds": [0214702358321752, 1752890679257238]
-    "sort": 0
-  },
-  "4526373295305388": {
-    "name": "Dream Journal",
-    "time": "2025-02-14",
-    "content": "I was flying over a city made of glass",
-    "memory": ["flying", "city made of glass"],
-    "memoryLength": 2,
-    "memoryIds": [4527307323225730, 7252124575123408]
-    "sort": 1
-  }
-}
-```
-
-### Memory List
+### Memories
 
 ```json
 {
@@ -43,5 +18,45 @@ npm run build
   "1752890679257238": "not to be",
   "4527307323225730": "flying",
   "7252124575123408": "city made of glass"
+}
+```
+
+### Events
+
+```json
+{
+  "9874122603798017": {
+    "name": "Philosophy Discussion",
+    "time": "2025-02-14",
+    "text": "To be, or not to be, that is the question",
+    "memory": "['to be', 'not to be']",
+    "memoryLength": 2,
+    "memoryIds": [0214702358321752, 1752890679257238],
+    "sort": 0
+  },
+  "4526373295305388": {
+    "name": "Dream Journal",
+    "time": "2025-02-14",
+    "text": "I was flying over a city made of glass",
+    "memory": "['flying', 'city made of glass']",
+    "memoryLength": 2,
+    "memoryIds": [4527307323225730, 7252124575123408],
+    "sort": 1
+  }
+}
+```
+
+### Topics
+
+```json
+{
+  "9874122603798017": {
+    "name": "Philosophy Discussion",
+    "time": "2025-02-14",
+    "memoryIdsRaw": "",
+    "memoryLength": 2,
+    "memoryIds": [0214702358321752, 1752890679257238],
+    "sort": 0
+  }
 }
 ```
--- app.vue
+++ app.vue
@@ -93,7 +93,7 @@
     <div class="flex flex-col flex-grow">
       <div
         class="flex flex-col flex-grow overflow-hidden rounded-lg"
-        v-if="eventId"
+        v-if="eventId || topicId"
       >
         <div class="flex min-h-11 rounded-t-lg overflow-hidden justify-between">
           <input
@@ -103,6 +103,7 @@
             class="h-full w-full focus:bg-stone-800 flex-grow px-7 pb-1 bg-stone-700 text-center text-xl text-stone-300 truncate hover:bg-stone-800"
           />
           <input
+            v-if="eventId"
             type="text"
             v-model="time"
             @input="onInput"
@@ -110,6 +111,7 @@
           />
         </div>
         <button
+          v-if="eventId"
           @click="copyToClipboard"
           class="w-full justify-self-end pb-1 self-end text-stone-400 bg-stone-700"
           :class="
@@ -120,7 +122,7 @@
         >
           copy
         </button>
-        <div class="flex">
+        <div v-if="eventId" class="flex">
           <button
             @click="setPaperToText"
             class="w-full justify-self-end pb-1 self-end text-stone-400 bg-stone-700"
@@ -153,14 +155,76 @@
           :style="{ backgroundPositionY }"
         ></textarea>
         <button
-          @click="removeText"
+          @click="eventId ? removeText() : removeTopic()"
           class="max-h-7 w-full bg-stone-700 justify-self-end text-stone-400 pb-1 hover:bg-stone-800 self-end hover:text-stone-300"
         >
           remove
         </button>
       </div>
     </div>
-    <div class="w-[250px]"></div>
+    <div class="w-[250px] flex flex-col gap-1">
+      <div class="flex-grow bg-circles bg-stone-500 rounded-lg overflow-hidden">
+        <div class="flex">
+          <button
+            @click="moveTopicDown"
+            class="max-h-7 bg-stone-700 pt-[3px] px-3 justify-self-end pb-1"
+            :class="
+              topicId === null || topic.sort === 0
+                ? 'cursor-default bg-slate-50 text-stone-500/60'
+                : 'hover:bg-stone-800 text-stone-400 hover:text-stone-300'
+            "
+          >
+            <IconArrow class="w-3 rotate-90" />
+          </button>
+          <button
+            @click="moveTopicUp"
+            class="max-h-7 bg-stone-700 pt-[3px] px-3 justify-self-end pb-1"
+            :class="
+              topicId === null || topic.sort === topicListSorted.length - 1
+                ? 'cursor-default bg-slate-50 text-stone-500/60'
+                : 'hover:bg-stone-800 text-stone-400 hover:text-stone-300'
+            "
+          >
+            <IconArrow class="w-3 -rotate-90" />
+          </button>
+          <button
+            @click="createTopic()"
+            class="bg-stone-700 w-full text-stone-400 hover:text-stone-300 pb-1 hover:bg-stone-800"
+          >
+            new
+          </button>
+          <div
+            class="bg-stone-700 text-stone-400 w-32 text-end pr-2 pt-[1px] cursor-default"
+          >
+            {{ totalTopicMemoryTokens || "" }}
+          </div>
+          <div
+            class="bg-stone-700 text-stone-400 w-20 text-end pr-2 pt-[1px] cursor-default"
+          >
+            {{ totalTopicMemories || "" }}
+          </div>
+        </div>
+        <div ref="topicListRef" class="flex-grow overflow-auto">
+          <div class="flex flex-col-reverse">
+            <button
+              v-for="[id, { name, memoryLength }] in topicListSorted"
+              class="flex py-[2px] text-left min-h-7 text-shadow outline-none text-stone-200 pr-2 gap-2 justify-between"
+              :class="
+                topicId === id
+                  ? 'pl-5 bg-gradient-to-r from-stone-600 to-transparent'
+                  : 'pl-3 hover:bg-gradient-to-r hover:from-stone-600/50 hover:to-transparent'
+              "
+              @click="toggleTopic(id)"
+            >
+              <span class="truncate">{{ name }}</span>
+              <div>
+                {{ memoryLength }}
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
@@ -169,24 +233,27 @@ import fileSave from "./utils/fileSave"
 import fileLoad from "./utils/fileLoad"
 import newId from "./utils/newId"
 import timestamp from "./utils/timestamp"
+import { onMounted } from "vue"
 const LOCAL_STORAGE_KEY = "stone"
 const AVERAGE_TOKENS = 42
 const COPY_DELAY = 200
 const eventListRef = ref(null)
 const paperRef = ref(null)
+const topicListRef = ref(null)
 
 const eventList = ref({})
+const memoryList = ref({})
+const topicList = ref({})
 const eventId = ref(null)
+const topicId = ref(null)
 
 // handle v-model fields to edit
 const name = ref("")
 const time = ref("")
 const paper = ref("")
 
-const PAPER_TYPES = { TEXT: false, MEMORY: true }
+const PAPER_TYPES = { TEXT: 0, MEMORY: 1, TOPIC: 2 }
 const activePaperType = ref(PAPER_TYPES.TEXT)
-
-const memoryList = ref({})
 
 let removed = null
 
@@ -194,10 +261,23 @@ const recentlyCopied = ref(false)
 const backgroundPositionY = ref("0px")
 const debouncedSaveLocalStorageItem = _.debounce(saveLocalStorageItem, 300)
 const debouncedUpdateMemoryList = _.debounce(updateMemoryList, 300)
+const debouncedUpdateTopicList = _.debounce(updateTopicList, 300)
 
 const event = computed(() => eventList.value[eventId.value])
+const topic = computed(() => topicList.value[topicId.value])
 const eventListSorted = computed(() => {
   return Object.entries(eventList.value).sort(([, a], [, b]) => a.sort - b.sort)
+})
+const topicListSorted = computed(() => {
+  return Object.entries(topicList.value).sort(([, a], [, b]) => a.sort - b.sort)
+})
+const totalTopicMemories = computed(() => {
+  return Object.values(topicList.value).reduce((sum, topic) => {
+    return sum + topic.memoryIds.length
+  }, 0)
+})
+const totalTopicMemoryTokens = computed(() => {
+  return totalTopicMemories.value * AVERAGE_TOKENS
 })
 
 onMounted(loadLocalStorageItem)
@@ -209,20 +289,42 @@ function createText() {
     time: new Date().toLocaleDateString(),
     text: "",
     memory: "", // valid JSON array
-    memoryLength: null,
     memoryIds: [],
+    memoryLength: null,
     sort: Object.keys(eventList.value).length,
   }
   toggleText(id)
   nextTick(() => {
     paperRef.value.focus()
-    eventListRef.value.scrollTop =
-      eventListRef.value.clientHeight - eventListRef.value.scrollHeight
+    scrollToTop(eventListRef)
   })
 }
+function createTopic() {
+  const id = newId()
+  topicList.value[id] = {
+    name: "topic",
+    memoryIdsRaw: "", // valid JSON array
+    memoryIds: [],
+    memoryLength: null,
+    sort: Object.keys(topicList.value).length,
+  }
+  toggleTopic(id)
+  nextTick(() => scrollToTop(topicListRef))
+}
 function toggleText(id) {
+  activePaperType.value = PAPER_TYPES.TEXT
   if (eventId.value === id) eventId.value = null
   else eventId.value = id
+  topicId.value = null
+  onTextScroll()
+  updateInputFields()
+  debouncedSaveLocalStorageItem()
+}
+function toggleTopic(id) {
+  activePaperType.value = PAPER_TYPES.TOPIC
+  if (topicId.value === id) topicId.value = null
+  else topicId.value = id
+  eventId.value = null
   onTextScroll()
   updateInputFields()
   debouncedSaveLocalStorageItem()
@@ -248,6 +350,11 @@ function updateInputFields() {
       ? (paper.value = eventCache.memory)
       : (paper.value = eventCache.text)
   }
+  const topicCache = topic.value
+  if (topicCache) {
+    name.value = topicCache.name
+    paper.value = topicCache.memoryIdsRaw
+  }
 }
 function onInput() {
   const eventCache = event.value
@@ -261,6 +368,12 @@ function onInput() {
       eventCache.text = paper.value
     }
   }
+  const topicCache = topic.value
+  if (topicCache) {
+    topicCache.name = name.value
+    topicCache.memoryIdsRaw = paper.value
+    debouncedUpdateTopicList(topicCache)
+  }
   debouncedSaveLocalStorageItem()
 }
 function updateMemoryList(event) {
@@ -268,7 +381,7 @@ function updateMemoryList(event) {
   event.memoryIds = []
   event.memoryLength = null
   try {
-    const parsedMemory = JSON.parse(paper.value)
+    const parsedMemory = JSON.parse(event.memory)
     if (Array.isArray(parsedMemory)) {
       parsedMemory.forEach((item) => {
         const id = newId()
@@ -289,6 +402,27 @@ function updateMemoryList(event) {
     }
   }
 }
+function updateTopicList(topic) {
+  topic.memoryIds = []
+  topic.memoryLength = null
+  try {
+    const parsedMemoryIds = JSON.parse(topic.memoryIdsRaw)
+    if (Array.isArray(parsedMemoryIds)) {
+      topic.memoryIds = parsedMemoryIds
+      topic.memoryLength = parsedMemoryIds.length
+    } else {
+      throw new Error("topic input must be a JSON array,")
+    }
+    console.log(`⏬ topic updated! [${timestamp()}]`)
+  } catch (error) {
+    const resetString = "topic has been reset"
+    if (error instanceof SyntaxError) {
+      console.log(`❗ invalid JSON format, ${resetString} [${timestamp()}]`)
+    } else {
+      console.log(`❗ ${error.message} ${resetString} [${timestamp()}]`)
+    }
+  }
+}
 function removeText() {
   removed = {}
   const id = eventId.value
@@ -301,14 +435,33 @@ function removeText() {
   })
   removed.event.memoryIds.forEach((id) => delete memoryList.value[id])
 }
+function removeTopic() {
+  removed = {}
+  const id = topicId.value
+  removed.topic = topic.value
+  removed.topicId = id
+  toggleTopic(id)
+  delete topicList.value[id]
+  Object.values(topicList.value).forEach((topic) => {
+    if (topic.sort > removed.topic.sort) topic.sort--
+  })
+}
 function restore() {
   if (!removed) return
-  eventList.value[removed.eventId] = {
-    ...removed.event,
-    sort: Object.keys(eventList.value).length,
+  if (removed.event) {
+    eventList.value[removed.eventId] = {
+      ...removed.event,
+      sort: Object.keys(eventList.value).length,
+    }
+    toggleText(removed.eventId)
+    updateMemoryList(eventList.value[removed.eventId])
+  } else {
+    topicList.value[removed.topicId] = {
+      ...removed.topic,
+      sort: Object.keys(topicList.value).length,
+    }
+    toggleTopic(removed.topicId)
   }
-  toggleText(removed.eventId)
-  updateMemoryList(eventList.value[removed.eventId])
   removed = null
   debouncedSaveLocalStorageItem()
 }
@@ -328,10 +481,18 @@ function moveTextUp() {
 function moveTextDown() {
   move(eventList.value, eventId.value, event.value, -1)
 }
+function moveTopicUp() {
+  move(topicList.value, topicId.value, topic.value, 1)
+}
+function moveTopicDown() {
+  move(topicList.value, topicId.value, topic.value, -1)
+}
 function getStorage() {
   return {
     eventList: eventList.value,
     eventId: eventId.value,
+    topicList: topicList.value,
+    topicId: topicId.value,
     memoryList: memoryList.value,
     activePaperType: activePaperType.value,
   }
@@ -346,6 +507,8 @@ function loadLocalStorageItem() {
 function injectStorage(storage) {
   eventList.value = storage.eventList
   eventId.value = storage.eventId
+  topicList.value = storage.topicList
+  topicId.value = storage.topicId
   activePaperType.value = storage.activePaperType
   memoryList.value = storage.memoryList
   updateInputFields()
@@ -356,6 +519,9 @@ function onTextScroll(event) {
     return
   }
   backgroundPositionY.value = `-${event.target.scrollTop}px`
+}
+function scrollToTop(ref) {
+  ref.value.scrollTop = ref.value.clientHeight - ref.value.scrollHeight
 }
 async function copyToClipboard() {
   if (recentlyCopied.value) return
