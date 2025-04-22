b3725d03ecd6a9e11dad14df872a8b85f197d16e
gukihuman <sokolovivanf@gmail.com>
2025-04-23 00:00:20 +0500

    fix echo name

--- pages/circle.vue
+++ pages/circle.vue
@@ -346,7 +346,7 @@ function assembleContext(forLLM = true) {
       contextString += "-----------------------------------------------\n\n"
     }
   })
-  if (forLLM) contextString += `<${entityName}>\n`
+  if (forLLM) contextString += `<echo>\n`
 
   estimatedCost.value = Math.floor(contextString.length / 3) + 500
   console.log(
