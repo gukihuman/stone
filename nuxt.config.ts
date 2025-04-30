81ebfcf2a8c6012ccaaada4e28e3b62d7d3aa47e
gukihuman <sokolovivanf@gmail.com>
2025-05-01 01:35:55 +0500

    add gen-test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 export default defineNuxtConfig({
-  nitro: { scanDirs: ["nitro-api", "server"] },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   modules: ["@nuxtjs/tailwindcss"],
