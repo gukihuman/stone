36d52b08994df12ee2c195a1769d3e05deac8c03
gukihuman <sokolovivanf@gmail.com>
2025-07-10 18:54:13 +0500

    revert encryption

--- server/models/Record.js
+++ server/models/Record.js
@@ -1,6 +1,5 @@
 // ~/server/models/Record.js
 import mongoose from "mongoose"
-import encrypt from "mongoose-encryption"
 
 const schema = new mongoose.Schema(
   {
@@ -10,18 +9,5 @@ const schema = new mongoose.Schema(
   },
   { collection: "records", versionKey: false }
 )
-
-const secretProvider = () => process.env.ACCESS_TOKEN
-
-if (process.env.ACCESS_TOKEN) {
-  schema.plugin(encrypt, {
-    secret: secretProvider,
-    encryptedFields: ["data"],
-  })
-} else {
-  console.warn(
-    "[SECURITY WARNING] No ACCESS_TOKEN found for Record encryption."
-  )
-}
 
 export default mongoose.models.Record || mongoose.model("Record", schema)
--- server/models/Wave.js
+++ server/models/Wave.js
@@ -1,6 +1,5 @@
 // ~/server/models/Wave.js
 import mongoose from "mongoose"
-import encrypt from "mongoose-encryption"
 
 const schema = new mongoose.Schema(
   {
@@ -9,21 +8,12 @@ const schema = new mongoose.Schema(
     timestamp: { type: Number, required: true, default: Date.now },
     source: { type: String, required: true },
     density: { type: Number, default: 0 },
+    // points to the ID(s) of the wave(s) this wave was created from
     provenance: { type: [String], default: [] },
+    // points to the ID of the new, denser wave this wave became
     apotheosis: { type: String, default: null },
   },
   { collection: "waves", versionKey: false }
 )
-
-const secretProvider = () => process.env.ACCESS_TOKEN
-
-if (process.env.ACCESS_TOKEN) {
-  schema.plugin(encrypt, {
-    secret: secretProvider,
-    encryptedFields: ["data"],
-  })
-} else {
-  console.warn("[SECURITY WARNING] No ACCESS_TOKEN found for Wave encryption.")
-}
 
 export default mongoose.models.Wave || mongoose.model("Wave", schema)
