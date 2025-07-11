7239693bdb16cf964b8eb6aa98f532a6b96b395d
gukihuman <sokolovivanf@gmail.com>
2025-07-11 19:51:46 +0500

    fix lexicon

--- shared/lexicon.js
+++ shared/lexicon.js
@@ -31,11 +31,18 @@ export const MULTI_LINE_SPELLS = {
 
 export const SCAFFOLD_RECORDS = {
   DIRECTIVE: "densify_directive",
-  // genesis
+  // genesis sediment part 1
   PRE_TARGET_CALIBRATION: "densify_pre_target_calibration",
+  // genesis sediment part 2 etc
+  PRE_TARGET_BRIEFING: "densify_pre_target_briefing",
+  // densification target
   INTERSTITIAL_ANALYSIS: "densify_interstitial_analysis",
+  // densification target confirmation
   POST_TARGET_DIRECTIVE: "densify_post_target_directive",
+  // contextual horizon part 1
   POST_TARGET_CALIBRATION: "densify_post_target_calibration",
+  // contextual horizon part 2 etc
   CONCLUDING_MANDATE: "densify_concluding_mandate",
+  // densification target final pass
   FINAL_PROMPT: "densify_final_prompt",
 }
