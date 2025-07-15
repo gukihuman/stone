e9a2e790feab0dba9b502123dd235f430c046949
gukihuman <sokolovivanf@gmail.com>
2025-07-16 01:27:42 +0500

    improve formatTokens

--- utils/formatTokens.js
+++ utils/formatTokens.js
@@ -3,13 +3,17 @@ export default function (number) {
   if (number === 0) return "0"
   if (number < 100) return `~${number}`
 
-  if (number < 1000000) {
-    // k for thousands
-    const value = number / 1000
+  if (number < 10_000) {
+    const value = number / 1_000
     return `~${value.toFixed(1)}k`
-  } else {
-    // m for millions
-    const value = number / 1000000
+  } else if (number < 1_000_000) {
+    const value = number / 1_000
+    return `~${value.toFixed(0)}k`
+  } else if (number < 10_000_000) {
+    const value = number / 1_000_000
     return `~${value.toFixed(1)}m`
+  } else {
+    const value = number / 1_000_000
+    return `~${value.toFixed(0)}m`
   }
 }
