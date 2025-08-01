3295d9aba0976389d024a4250676211f8a3b97d0
gukihuman <sokolovivanf@gmail.com>
2025-08-01 23:10:54 +0500

    fix baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // ~/nuxt.config.ts
-const devBaseUrl = "stone-git-core-gukis-projects.vercel.app"
-// const devBaseUrl = "stone-seven.vercel.app" // want to restore
+const devBaseUrl = "lithos-git-core-gukis-projects.vercel.app"
+// const devBaseUrl = "lithos-smth.vercel.app" // want to restore, make a good name there
 
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -9,8 +9,6 @@ export default defineNuxtConfig({
   css: ["./main.css"],
   runtimeConfig: {
     public: {
-      human: process.env.HUMAN || "Human", // legacy i think
-      entities: process.env.ENTITIES?.split(",").map((e) => e.trim()), // also legacy pretty sure
       baseUrl: `https://${process.env.NUXT_PUBLIC_BASE_URL || devBaseUrl}`,
     },
   },
